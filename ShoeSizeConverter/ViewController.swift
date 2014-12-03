f674492928ce828e577176129226f69642698ad9
Matthew Surowiec <mp5auc3@gmail.com>
2014-12-03 14:30:28 -0500

    added convert button functionality and refactored

--- ShoeSizeConverter/ViewController.swift
+++ ShoeSizeConverter/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+//        let sizeFromTextField = mensShoeSizeTextField.text
+//        let numberFromTextField = sizeFromTextField.toInt()
+//        var integerFromTextField = numberFromTextField!
+//integerFromTextField += conversionConstant
+//let stringWithUpdatedShoeSize = "\(integerFromTextField)"
+//mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize
+        
+        
+        let sizeFromTextField = mensShoeSizeTextField.text.toInt()!
+        mensConvertedShoeSizeLabel.hidden = false
+        let conversionConstant = 30
+        mensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + "In European Shoe Size"
+        
+        
+
+        
+    }
 
 }
 
